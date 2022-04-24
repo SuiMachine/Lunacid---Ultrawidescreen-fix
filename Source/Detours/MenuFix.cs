87bc8a6232e107568bc679689d15ec66c0e3a627
SuiMachine <SuiMachine@users.noreply.github.com>
2022-04-24 19:41:55 +0200

    Release files

---
+++ Source/Detours/MenuFix.cs
@@ -0,0 +1,32 @@
+﻿using HarmonyLib;
+using UnityEngine.UI;
+
+namespace LunacidUltrawidescreenFix.Detours
+{
+	[HarmonyPatch]
+	public class MenuFix
+	{
+		[HarmonyPostfix]
+		[HarmonyPatch(typeof(Menus), nameof(Menus.LoadMenu))]
+		public static void LoadMenuDetour(Menus __instance)
+		{
+			Plugin.LogInstance.LogError("No Canvas scaler in parent!");
+
+			var parent = __instance.transform.parent;
+			if(parent == null)
+			{
+				Plugin.LogInstance.LogError("No MainMenu parent!");
+				return;
+			}
+
+			var canvasScaler = parent.GetComponent<CanvasScaler>();
+			if(canvasScaler == null)
+			{
+				Plugin.LogInstance.LogError("No Canvas scaler in parent!");
+				return;
+			}
+
+			canvasScaler.screenMatchMode = CanvasScaler.ScreenMatchMode.Expand;
+		}
+	}
+}
---
+++ Source/Plugin.cs
@@ -0,0 +1,21 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace LunacidUltrawidescreenFix
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static Harmony Harmony { get; private set; }
+        public static BepInEx.Logging.ManualLogSource LogInstance { get; private set; }
+
+        private void Awake()
+        {
+            LogInstance = Logger;
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} initializing!");
+            Harmony = new Harmony("local.ultrawidescreenfix.suimachine");
+            Harmony.PatchAll();
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+    }
+}
